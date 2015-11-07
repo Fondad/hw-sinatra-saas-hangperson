2aacb0b3581c8cfa30dc48d631b561ef7f062cf4
fondad <fondad@ukr.net>
2015-11-07 10:45:00 +0000

    App HangpersonGame is ready

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,47 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  #it processes a guess and modifies the instance variables wrong_guesses and guesses accordingly
+  def guess letter
+    unless letter =~ /\p{Alpha}/i
+      raise ArgumentError
+    else
+      unless (@guesses+@wrong_guesses).include? letter.downcase
+        if @word.include? letter
+          @guesses << letter
+        else
+          @wrong_guesses << letter
+        end
+        return true
+      end
+    return false
+    end
+  end
+  
+  #it returns one of the symbols :win, :lose, or :play depending on the current game state
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length >= 7
+    if self::word_with_guesses == @word 
+      return :win
+    else 
+      return :play
+    end
+  end
+  
+  #it substitutes the correct guesses made so far into the word
+  def word_with_guesses
+    @word.tr("^#{@guesses}.", '-')
+    #@word.gsub(/[^@guesses]/, '-')
   end
 
   def self.get_random_word
